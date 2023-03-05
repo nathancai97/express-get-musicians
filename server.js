2615eca5b3ee122c23b286a0a75c81bc4e4db69e
Nathan Cai <nathan97cai@gmail.com>
2023-03-05 17:03:10 -0500

    successfully hit endpoint, tested on postman

--- server.js
+++ server.js
@@ -16,3 +16,8 @@ app.get('/musicians', async (req, res) => {
     const data = await Musician.findAll();
     return res.json(data);
 });
+
+app.get('/musicians/:id', async (req, res) => {
+    const data = await Musician.findByPk(req.params.id);
+    return res.json(data);
+})
