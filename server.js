9f7ff00424fd48ab6c762f73ac991b654dd59c5e
Nathan Cai <nathan97cai@gmail.com>
2023-03-05 16:44:32 -0500

    successfuly hit musicians get endpoint

--- server.js
+++ server.js
@@ -11,3 +11,8 @@ app.listen(port, () => {
     sequelize.sync();
     console.log(`Listening on port ${port}`)
 })
+
+app.get('/musicians', async (req, res) => {
+    const data = await Musician.findAll();
+    return res.json(data);
+});
